3d9eb6b64839866dac3bc447c11442d5d60d08f9
amitha :) <abhi@macbook-pro.local>
2026-06-05 14:31:18 +0530

    fic CORS for Vercel again

--- server/index.js
+++ server/index.js
@@ -8,10 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://project-france-a4iz.vercel.app'
-  ],
+  origin: true,
   credentials: true
 }));
 
